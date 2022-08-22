ea472156b49c7a7c24a3201eaead11a3a242d767
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-08-22 14:13:46 -0600

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # route to test your configuration
   get '/hello', to: 'application#hello_world'
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
