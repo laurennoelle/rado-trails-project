29f7ecad38255b2e2675b0617ad98c3df6daf594
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-08-25 16:26:06 -0600

    stuck on review

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,18 @@
 #   Character.create(name: "Luke", movie: movies.first)
 puts "🌱 Seeding trails..."
 
-u1 = User.create(username: "lnoelle", password: "12345678")
-u2 = User.create(username: "csale", password: "87654321")
 
-t1 = Trail.create(name:"Horsetooth Trail", img_url:"https://images.unsplash.com/photo-1658419655425-d359ac8a0bf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", city:"Fort Collins", length: 4, est_time: 2, map:"https://www.larimer.gov/naturalresources/parks/horsetooth-mountain",likes: 4)
-t2 = Trail.create(name:"Hewlett Gultch", img_url:"https://images.unsplash.com/photo-1590013335840-83bbe7031d4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1941&q=80", city:"Fort Collins", length: 8, est_time: 3, map:"https://www.fs.usda.gov/recarea/arp/recarea/?recid=36779",likes: 5)
-t3 = Trail.create(name:"Grey Rock", img_url:"https://images.unsplash.com/photo-1619994948937-ef1e758d46ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80", city:"Fort Collins", length: 9, est_time: 5, map:"https://www.fs.usda.gov/recarea/arp/recarea/?recid=36777",likes: 10)
-t4 = Trail.create(name:"Bear Peak", img_url:"https://images.unsplash.com/photo-1528226922624-a3d2fe277cf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80", city:"Boulder", length: 3, est_time: 3, map: "https://bouldercolorado.gov/trail/bear-peak",likes: 11)
-t5 = Trail.create(name:"Mesa Trail", img_url:"https://images.unsplash.com/photo-1644983038252-a80b1536bdfb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80", city:"Boulder", length: 2, est_time: 3, map:"https://bouldercolorado.gov/trail/mesa",likes: 6)
-t6 = Trail.create(name:"Resevoir Ridge", img_url:"https://images.unsplash.com/photo-1653250947541-756cf6d786f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80", city:"Fort Collins", length: 4, est_time: 2, map:"https://www.fcgov.com/naturalareas/finder/reservoirridge",likes: 10)
+ Trail.create(name:"Horsetooth Trail", img_url:"https://images.unsplash.com/photo-1658419655425-d359ac8a0bf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", city:"Fort Collins", length: 4, est_time: 2, map:"https://www.larimer.gov/naturalresources/parks/horsetooth-mountain",likes: 4)
+Trail.create(name:"Hewlett Gultch", img_url:"https://images.unsplash.com/photo-1590013335840-83bbe7031d4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1941&q=80", city:"Fort Collins", length: 8, est_time: 3, map:"https://www.fs.usda.gov/recarea/arp/recarea/?recid=36779",likes: 5)
+Trail.create(name:"Grey Rock", img_url:"https://images.unsplash.com/photo-1619994948937-ef1e758d46ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80", city:"Fort Collins", length: 9, est_time: 5, map:"https://www.fs.usda.gov/recarea/arp/recarea/?recid=36777",likes: 10)
+Trail.create(name:"Bear Peak", img_url:"https://images.unsplash.com/photo-1528226922624-a3d2fe277cf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80", city:"Boulder", length: 3, est_time: 3, map: "https://bouldercolorado.gov/trail/bear-peak",likes: 11)
+Trail.create(name:"Mesa Trail", img_url:"https://images.unsplash.com/photo-1644983038252-a80b1536bdfb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80", city:"Boulder", length: 2, est_time: 3, map:"https://bouldercolorado.gov/trail/mesa",likes: 6)
+Trail.create(name:"Resevoir Ridge", img_url:"https://images.unsplash.com/photo-1653250947541-756cf6d786f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80", city:"Fort Collins", length: 4, est_time: 2, map:"https://www.fcgov.com/naturalareas/finder/reservoirridge",likes: 10)
 
-Review.create(username: "lnoelle" ,comment: "This trial had great spring flowers and is dog friendly!", rating: 3, trail_id: t1.id, user_id: u1.id )
+User.create(username: "lnoelle", password: "12345678")
+User.create(username: "csale", password: "87654321")
+
+Review.create(username: "lnoelle", comment: "This trial had great spring flowers and is dog friendly!", rating: 3)
 Review.create(username: "lnoelle" ,comment: "Challenging and muddy!", rating: 1, trail_id: t4.id, user_id: u1.id )
 Review.create(username: "lnoelle" ,comment: "Gorgeous trail with goats!", rating: 3, trail_id: t2.id, user_id: u1.id )
 Review.create(username: "csale" ,comment: "Loved the views at the top!", rating: 4, trail_id: t3.id, user_id: u2.id )
